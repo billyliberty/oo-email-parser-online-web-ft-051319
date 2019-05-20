e121f9bcaf8512ea4f5484b141b2b52a3f319bac
Joshua Crum <joshua.crum@gmail.com>
2019-05-20 17:20:05 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,2 +1,12 @@
 class EmailParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    @emails = @emails.gshub(/, " ").split
+    @emails.uniq
+  end
 end
